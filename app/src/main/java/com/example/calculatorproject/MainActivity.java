6576c76e325db3ed279b9b04fe254994af059d32
JonMoyy <90794057+JonMoyy@users.noreply.github.com>
2022-08-22 08:18:40 -0500

    fix code for power button

--- app/src/main/java/com/example/calculatorproject/MainActivity.java
+++ app/src/main/java/com/example/calculatorproject/MainActivity.java
@@ -9,13 +9,17 @@ import android.widget.TextClock;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+    sources:
+    https://www.tutorialkart.com/kotlin-android/kotlin-android-button-background-color/
 
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void findSum(View view){
+    public void findAdd(View view){
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
@@ -26,4 +30,56 @@ public class MainActivity extends AppCompatActivity {
 
         numberSumTV.setText("" + sum);
     }
+
+    public void findSub(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = num1 - num2;
+
+        numberSumTV.setText("" + sum);
+    }
+
+    public void findDiv(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = num1 / num2;
+
+        numberSumTV.setText("" + sum);
+    }
+
+    public void findMulti(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = num1 * num2;
+
+        numberSumTV.setText("" + sum);
+    }
+
+
+    public void findPower(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = ;
+
+        numberSumTV.setText("" + sum);
+    }
+
+
+
 }
